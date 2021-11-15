295ca388d14fa061d31388fbe0d63a35a657e4b0
syedasamia <samia054@gmail.com>
2021-11-15 21:53:18 +0100

    controller and app have been separated

---
+++ app/controller/controller.js
@@ -0,0 +1,5 @@
+const app = require(__dirname + "/app.js");
+
+// Listen for incoming requests
+const PORT = process.env.PORT || 3000
+app.listen(PORT, console.log(`server started, listening PORT ${PORT}`))
