a223e90fd7b00cfe52175df2a18b88f2934beaa6
syedasamia <samia054@gmail.com>
2021-11-25 12:36:59 +0100

    Bug fixed

--- app/domain/url-shortener.js
+++ app/domain/url-shortener.js
@@ -3,8 +3,8 @@ const hashids = new Hashids("das ist mein salz", 7)
 
 
 function generateUrlId(){
-    const urlId = hashids.encode(Date.now)
-    console.log(urlId)
+    const urlId = hashids.encode(Date.now())
+    console.log("XYZ from domain " + urlId)
 
     return urlId;
 }
