f5c0f7bd8ddd58a7a2ecb9915c9d82eb638ccf52
syedasamia <samia054@gmail.com>
2021-11-25 15:55:35 +0100

    db.config.js mongoose error propable fix

--- app/infrastructure/configuration/db.config.js
+++ app/infrastructure/configuration/db.config.js
@@ -6,7 +6,11 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.ATLAS_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 //Get the default connection
-const db = mongoose.connection;
+const db = mongoose.connection.once('open', function(){
+    console.log('Conection has been made!');
+        }).on('error', function(error){
+     console.log('Error is: ', error);
+      });
 
 //Bind connection to error event (to get notification of connection errors)
 //db.on('error', console.error.bind(console, 'MongoDB connection error:'));
