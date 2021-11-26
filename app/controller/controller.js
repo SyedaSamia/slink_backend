92c7a1126edfdfdaaf601f66a47a12ace508d212
syedasamia <samia054@gmail.com>
2021-11-26 22:11:32 +0100

    added prod env checking

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -2,6 +2,14 @@
 
 const app = require(__dirname + "/app.js")
 
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+
+
 // Listen for incoming requests
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`server started, listening PORT ${PORT}`))
