a3caa1d225189e4ef6fc8d122c9a769cb8a0404e
syedasamia <samia054@gmail.com>
2021-11-26 17:35:36 +0100

    Enabled other options for http reqs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ connection.on('error', () => console.log('Error.....!!!'))
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
    // res.header('Access-Control-Request-Method', 'POST');
    // res.header('Access-Control-Request-Headers', 'Content-Type, Authorization');
