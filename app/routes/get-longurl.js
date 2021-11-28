37315f47ba3e87670f73e50a9cf6179059a5085c
syedasamia <samia054@gmail.com>
2021-11-28 18:43:20 +0100

    minor bug fixed

--- app/routes/get-longurl.js
+++ app/routes/get-longurl.js
@@ -4,24 +4,24 @@ const router = express.Router()
 
 const Url = require('../domain/models/model')
 
-router.get('/:urlId', async (req, res) => {
+router.get('/:code', async (req, res) => {
     console.log("XYZ1" )
     try{
         // find a document match to the code in req.params.code
         console.log("XYZ2" + url)
         const url = await Url.findOne({
-            urlId: req.params.urlId
+            urlId: req.params.code
         })
         console.log("XYZ3" + url)
         if (url){
             // count the visit of that shortUrl
             url.shortUrlRedirCount++
             await url.save()
-            res.redirect(url.longUrl)
+            return res.redirect(url.longUrl)
         }
         else {
             console.log("XYZ4" + url)
-            res.status(404).json('No Url Found!')
+            return res.status(404).json('No Url Found!')
         }
 
     }
